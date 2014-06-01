33aea65835a9a531938dadb4499d37bb0071b0d5
Emanuel Borsboom <manny@fpcomplete.com>
2014-06-01 19:42:29 +0000

    Piggies

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,27 @@
+{-# LANGUAGE MultiParamTypeClasses, OverloadedStrings, QuasiQuotes, TemplateHaskell, TypeFamilies #-}
 module Main where
 
-main = putStrLn "Welcome to the FP Haskell Center!"
+import Yesod
+
+data Piggies = Piggies
+
+instance Yesod Piggies
+
+mkYesod "Piggies" [parseRoutes|
+  /      HomeR  GET
+  /about AboutR GET
+|]
+
+getHomeR = defaultLayout [whamlet|
+  Welcome to the Pigsty!
+  <br>
+  <a href=@{AboutR}>About Us.
+|]
+
+getAboutR = defaultLayout [whamlet|
+  Enough about us!
+  <br>
+  <a href=@{HomeR}>Back Home.
+|]
+
+main = warpEnv Piggies
